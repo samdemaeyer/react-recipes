ea1cd483589820f1b22f3005e26f7303185a344f
Sam De Maeyer <sam_demaeyer@hotmail.com>
2021-06-12 16:23:34 +0100

    Add Mutation to schema

--- graphql-types/root-mutuaion.js
+++ graphql-types/root-mutuaion.js
@@ -1,7 +1,29 @@
-// const { GraphQLObjectType } = require('graphql')
+const { GraphQLObjectType, GraphQLNonNull, GraphQLString } = require('graphql')
+const { recipes } = require('../db')
+const { RecipeType } = require('./recipe')
 
-// const RootMutuaionType = new GraphQLObjectType({})
+const RootMutuaionType = new GraphQLObjectType({
+  name: 'Mutation',
+  description: 'Root Mutation',
+  fields: () => ({
+    addRecipe: {
+      type: RecipeType,
+      description: 'Add a recipe',
+      args: {
+        title: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (_, args) => {
+        const recipe = {
+          id: recipes.length + 1,
+          ...args,
+        }
+        recipes.push(recipe)
+        return recipe
+      },
+    },
+  }),
+})
 
 module.exports = {
-  // RootMutuaionType,
+  RootMutuaionType,
 }
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const app = require('express')()
 const { graphqlHTTP } = require('express-graphql')
 const { GraphQLSchema } = require('graphql')
-const { RootQueryType } = require('./graphql-types')
+const { RootQueryType, RootMutuaionType } = require('./graphql-types')
 
 const schema = new GraphQLSchema({
   query: RootQueryType,
+  mutation: RootMutuaionType,
 })
 
 app.use('/graphql', graphqlHTTP({ graphiql: true, schema }))
